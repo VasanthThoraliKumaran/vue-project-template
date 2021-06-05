578e08cc1361fd4f75e3ffba608b1a49312d5175
VasanthThorazhiKumaran <vasanthtk737@gmail.com>
2021-06-05 21:11:32 +0530

    fixed import

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import NotFound from '../views/NotFound.vue';
+import NotFound from "../fallback-view/NotFound.vue";
 
 Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   {
     path: "*",
     component: NotFound,
-  }
+  },
 ];
 
 const router = new VueRouter({

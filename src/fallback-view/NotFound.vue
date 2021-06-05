f2b12cde2d541c38637535261a0c79fdff0fac1d
VasanthThorazhiKumaran <vasanthtk737@gmail.com>
2021-06-05 21:11:39 +0530

    updated

---
+++ src/fallback-view/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="d-flex flex-column justify-center align-center not-found">
+    <v-icon color="white" x-large>mdi-emoticon-frown</v-icon>
+    <h1>Page Not Found</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+  background-color: #4b4453;
+  color: white;
+  height: 100%;
+}
+</style>

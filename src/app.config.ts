49cae277f1c9a330aeaff7fa9d372428cc9ee76a
VasanthThorazhiKumaran <vasanthtk737@gmail.com>
2021-07-15 19:37:39 +0530

    added themes

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,13 +1,28 @@
+import { colors } from "vuetify/lib";
+import { VuetifyThemeVariant } from "vuetify/types/services/theme";
+
 export const appConfig = {
-    title: 'EerahZone',
-    version: process.env.VERSION,
-    versionCode: process.env.VERSION_CODE,
-    port: {
-        dev_server: 8080,
-    },
-    mode: {
-        isDevelopment: process.env.NODE_ENV === 'development',
-        isProduction: process.env.NODE_ENV === 'production',
-        isTest: process.env.NODE_ENV === 'test',
-    }
-}
+  title: "EerahZone",
+  version: process.env.VERSION,
+  versionCode: process.env.VERSION_CODE,
+  port: {
+    dev_server: 8080,
+  },
+  mode: {
+    isDevelopment: process.env.NODE_ENV === "development",
+    isProduction: process.env.NODE_ENV === "production",
+    isTest: process.env.NODE_ENV === "test",
+  },
+};
+
+export const lightTheme: Partial<VuetifyThemeVariant> = {
+  primary: colors.grey.darken3, // #E53935
+  secondary: colors.shades.white, // #FFCDD2
+  accent: colors.grey.base, // #3F51B5
+};
+
+export const darkTheme: Partial<VuetifyThemeVariant> = {
+  primary: colors.grey.darken1, // #E53935
+  secondary: colors.shades.white, // #FFCDD2
+  accent: colors.grey.base, // #3F51B5
+};

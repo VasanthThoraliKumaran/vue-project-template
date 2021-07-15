20af2d44a888e4f60f4dd74b17cecfc8894c150e
VasanthThorazhiKumaran <vasanthtk737@gmail.com>
2021-07-15 19:38:26 +0530

    instantiated vuetify

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,32 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { appService } from "./app.service";
+export default class App extends Vue {
+  public static instance: App;
 
-export default Vue.extend({
-  name: "App",
-});
+  constructor() {
+    super();
+    App.instance = this;
+    appService.setDarkTheme(false);
+  }
+
+  public getVuetifyInstance() {
+    return this.$vuetify;
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+@import "/src/scss/global.scss";
+@import "/src/scss/variables.scss";
+@import "/src/scss/fonts.scss";
 #app {
   font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
-  min-height: 100%;
+  height: 100% !important;
+  min-height: 100% !important;
+  min-width: 300px !important;
+  scroll-behavior: smooth;
 }
 </style>
